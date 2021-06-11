30b4aaa4cd4ebe95f23f3fa8b096d2e1e14e34b9
mithileshkumaar <mithilesh.kumaar@yahoo.com>
2021-06-11 14:56:44 +0530

    commit-me

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,61 @@
 <template>
   <div id="home">
     <div class="row">
-      <div class="col-4" id="SearchComponent">
+      <div class="col-4" id="SearchComponent" style="padding-left:40px; width:600px;">
         <!-- Mithilesh's work area -->
+      <div class="container card " style="background-color:#171616; padding-top:30px; " >
+    
+    <div class="d-flex flex-row justify-content-center" style="width:auto;">
+  <div class="p-2">
+    
+    <a href="#">
+        <button type="button" class="btn btn-outline-success" style="width:auto;">people</button>
+       </a></div>
+  
+  <div class="p-2">
+    
+    <a href="#">
+        <button type="button" class="btn btn-outline-success" style="width:auto;">group</button>
+       </a> </div>
+       
+       </div>
+
+      <div class="card-body" style="padding-right:25px;">
+       <form class=" container d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color:black; color:white; border-radius:10px;">
+        
+      </form>
+
+      <div class=" card-body" >
+     <div class=" container card text-center " style="width: 18rem; border-radius: 30px; background-color:#2D2A2A; color:#fff; padding:20px; padding-left:25px; width:auto">
+  <h5 class="card-title">Studentcare attrocities<br><small>29 members</small></h5>
+    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo laborum. Ducimus nemo eum voluptatibus recusandae quibusdam maiores deserunt, quam, molestias sunt libero vitae eveniet autem odit animi consequatur harum.</p>
+    
+  
+</div>
+
+
+<div class=" container card text-center" style="width: 18rem; border-radius: 30px; background-color:#2D2A2A; color:#fff; padding:20px; width:auto;">
+  
+    <h5 class="card-title">7th floor fuckers</h5>
+    <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem dolores laboriosam, doloremque, dolorem similique, exercitationem velit nostrum voluptas labore harum quibusdam. Consequatur, laborum recusandae? Minima sed praesentium tenetur asperiores ullam.</p>
+    
+  
+</div>
+</div>
+       </div>
+
+
       </div>
+    </div>
+
+
+
+
+
+
+
+      
       <div class="col-8" id="PostsComponent">
         <!-- Krishna's work area -->
       </div>
@@ -18,4 +70,28 @@ export default {
 };
 </script>
 <style scoped>
+
+.card{
+ 
+  margin: 20px;
+border-radius: 30px;
+}
+
+
+.btn{ 
+  
+   border-radius:300px;
+  
+}
+
+
+.container{
+  width: auto;
+}
+
+
+
+
+
+
 </style>
