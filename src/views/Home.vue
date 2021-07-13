456f2d30e172bde8ae02b069f52441a0a7843ec5
mithileshkumaar <mithilesh.kumaar@yahoo.com>
2021-07-13 09:10:43 +0530

    home-page-new-change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,12 @@
 <template>
   <div id="home">
+    
     <div class="row">
+      <div class="d-flex flex-row bd-highlight mb-3">
       <div class="col-4" id="SearchComponent" style="padding-left:40px; width:600px;">
-        <!-- Mithilesh's work area -->
-      <div class="container card " style="background-color:#171616; padding-top:30px; " >
+        <!-- Mithilesh's work place -->
+        <div class="p-2 bd-highlight">
+      <div class="container card " style="background-color:#171616; padding-top:30px; margin-left:0px; " >
     
     <div class="d-flex flex-row justify-content-center" style="width:auto;">
   <div class="p-2">
@@ -21,27 +24,47 @@
        </div>
 
       <div class="card-body" style="padding-right:25px;">
-       <form class=" container d-flex">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color:black; color:white; border-radius:10px;">
+       <form class=" container d-flex" style="width:500px; margin-left:-20px;">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search" style="background-color:black; color:white; border-radius:10px; ">
         
       </form>
 
       <div class=" card-body" >
-     <div class=" container card text-center " style="width: 18rem; border-radius: 30px; background-color:#2D2A2A; color:#fff; padding:20px; padding-left:25px; width:auto">
-  <h5 class="card-title">Studentcare attrocities<br><small>29 members</small></h5>
-    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias, nemo laborum. Ducimus nemo eum voluptatibus recusandae quibusdam maiores deserunt, quam, molestias sunt libero vitae eveniet autem odit animi consequatur harum.</p>
+     
+
+<div class=" container card text-center" style="width: 500px; border-radius: 20px; background-color:#272626; color:#fff;  padding:10px; padding-left:25; margin-left:-45px;">
+  
+    <h5 class="card-title"><img src="../assets/headache.png" alt="" style="border-radius:1000px; height:30px; justify-content:start; margin-right:400px"> </h5>
+    
+    
+  
+</div>
+<div class=" container card text-center " style="width: 500px; border-radius: 20px; background-color:#272626; color:#fff; padding:10px; padding-left:25; margin-left:-45px; ">
+  <h5 class="card-title"><img src="../assets/headache.png" alt="" style="border-radius:1000px; height:30px; justify-content:start; margin-right:400px"><br></h5>
+    
+    
+  
+</div>
+<div class=" container card text-center " style="width: 500px; border-radius: 20px; background-color:#272626; color:#fff; padding:10px; padding-left:25; margin-left:-45px; ">
+  <h5 class="card-title"><img src="../assets/headache.png" alt="" style="border-radius:1000px; height:30px; justify-content:start; margin-right:400px"><br></h5>
+    
+    
+  
+</div>
+<div class=" container card text-center " style="width: 500px; border-radius: 20px; background-color:#272626; color:#fff; padding:10px; padding-left:25; margin-left:-45px; ">
+  <h5 class="card-title"><img src="../assets/headache.png" alt="" style="border-radius:1000px; height:30px; justify-content:start; margin-right:400px"><br></h5>
+    
+    
+  
+</div>
+<div class=" container card text-center " style="width: 500px; border-radius: 20px; background-color:#272626; color:#fff; padding:10px; padding-left:25; margin-left:-45px; ">
+  <h5 class="card-title">Studentcare attrocities<br></h5>
+    <p class="card-text">hi</p>
     
   
 </div>
 
 
-<div class=" container card text-center" style="width: 18rem; border-radius: 30px; background-color:#2D2A2A; color:#fff; padding:20px; width:auto;">
-  
-    <h5 class="card-title">7th floor fuckers</h5>
-    <p class="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatem dolores laboriosam, doloremque, dolorem similique, exercitationem velit nostrum voluptas labore harum quibusdam. Consequatur, laborum recusandae? Minima sed praesentium tenetur asperiores ullam.</p>
-    
-  
-</div>
 </div>
        </div>
 
@@ -54,13 +77,40 @@
 
 
 
-
+      </div>
       
       <div class="col-8" id="PostsComponent">
         <!-- Krishna's work area -->
+        <div class="d-flex">
+        <div class="p-2 bd-highlight">
+          
+        <div class="card" style="width:700px; background-color:#171616;">
+
+  <div class="card-body" style="padding-top:-100px; margin-top:10px; ">
+    <h3 style="color:white;">working on my html</h3>
+  </div>
+
+  <div class="card-body" style=" border-bottom-right-radius: 25px; border-bottom-left-radius: 25px; background-color:#272626;margin-top:0px; padding:1px; ">
+    <div class="d-flex flex-row bd-highlight mb-3">
+ <div class="p-2 bd-highlight">
+    <img src="../assets/headache.png" alt="" style="border-radius:1000px; height:30px; justify-content:start; margin-right:400px"> </div>
+     
+    
+    </div>
+  </div>
+</div>
+        <img src="../assets/IPL2.jpg" alt="" style="height:400px; width:700px; border-radius:50px;">
+
+          </div>
+
+
       </div>
     </div>
   </div>
+    </div>
+  </div>
+
+  
 </template>
 
 <script>
@@ -90,7 +140,10 @@ border-radius: 30px;
 }
 
 
-
+#PostsComponent img{
+  
+  
+}
 
 
 
